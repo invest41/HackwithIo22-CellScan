a34cfb1fad8fffa976550ccd939cd2e34d6cae22
invest41 <invest41@outlook.com>
2022-07-14 01:33:27 +0100

    Add working files

--- static/js/main.js
+++ static/js/main.js
@@ -7,13 +7,17 @@ $(document).ready(function () {
     // Upload Preview
     function readURL(input) {
         if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
+            // $('#my_image').attr('src','second.jpg');
+            // var reader = new FileReader();
+            // reader.onload = function (e) {
+            //     console.log(e)
+            //     $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+            //     $('#imagePreview').hide();
+            //     $('#imagePreview').fadeIn(650);
+            // }
+            // reader.readAsDataURL(input.files[0]);
+           const img =  document.getElementById('image-output')
+           img.src = URL.createObjectURL($('input[type=file]')[0].files[0])
         }
     }
     $("#imageUpload").change(function () {
@@ -26,7 +30,8 @@ $(document).ready(function () {
 
     // Predict
     $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
+        var form_data = new FormData();
+        form_data.append("image", $('input[type=file]')[0].files[0]);
 
         // Show loading animation
         $(this).hide();
@@ -35,7 +40,7 @@ $(document).ready(function () {
         // Make prediction by calling api /predict
         $.ajax({
             type: 'POST',
-            url: '/predict',
+            url: 'http://35.239.79.117/predict',
             data: form_data,
             contentType: false,
             cache: false,
@@ -45,6 +50,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
+                // $('#result').text(' Result:  ' + JSON.stringify(data));
                 $('#result').text(' Result:  ' + data);
                 console.log('Success!');
             },
